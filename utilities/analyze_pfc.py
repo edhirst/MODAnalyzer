74ff0745f85e8ecad13b87d7c6f636f176fcddd2
prekshan <33843655+prekshan@users.noreply.github.com>
2018-06-14 17:35:46 -0400

    Add files via upload

--- utilities/analyze_pfc.py
+++ utilities/analyze_pfc.py
@@ -5,23 +5,30 @@ import sys
 from collections import defaultdict
 
 input_path = sys.argv[1]
-output_file = sys.argv[2]
+output_path = sys.argv[2]
+type_of_data = sys.argv[3]
 
-def run_analyzer(input_path, output_file):
-  to_analyze = []
-  for f in os.listdir(input_path):
-    if f.endswith("mod"):
-      to_analyze.append(f)
-
-  to_analyze.sort()
-
-  for f in to_analyze:
-    call(['./bin/analyze_pfc', input_path + f, output_file])
-
+def run_analyzer(input_path, output_path, type_of_data):
+  to_analyze_and_output = []
+  if type_of_data == "2011":
+    for file in os.listdir(input_path):
+      if file.endswith("mod"):
+        to_analyze_and_output.append((input_path + "/" + file, output_path + "/" + file.replace(".mod", ".dat")))
+  elif type_of_data == "sim":
+    all_pythia_runs = os.listdir(input_path)
+    for pythia_run in all_pythia_runs:
+        print(pythia_run)
+        for dirName, subdirList, files in os.walk(input_path + pythia_run):
+            for file in files:
+                if file.endswith("mod"):
+                    to_analyze_and_output.append((dirName + "/" + file, output_path + "/" + pythia_run + file.replace(".mod", ".dat")))
+  for input, output in to_analyze_and_output:
+    args = ['./bin/analyze_pfc', input, output ] + sys.argv[3:]
+    call(args)
 
 start = time()
 
-run_analyzer(input_path, output_file)
+run_analyzer(input_path, output_path, type_of_data)
 
 end = time()
 
