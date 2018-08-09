bbea2b746557cb3aae1f679f14f83e9f8184bb82
prekshan <33843655+prekshan@users.noreply.github.com>
2018-08-09 11:56:58 -0400

    Add files via upload

--- src/InfoPFC.cc
+++ src/InfoPFC.cc
@@ -4,7 +4,7 @@ using namespace std;
 using namespace fastjet;
 
 
-MOD::InfoPFC::InfoPFC(int pdgId, string tag) : _pdgId(pdgId), _tag(tag) {}
+MOD::InfoPFC::InfoPFC(int pdgId, string tag, int vertex) : _pdgId(pdgId), _tag(tag), _vertex(vertex){}
 
 const int MOD::InfoPFC::pdgId() const {
   return _pdgId;
@@ -13,6 +13,11 @@ const int MOD::InfoPFC::pdgId() const {
 const string MOD::InfoPFC::tag() const {
   return _tag;
 }
+
+const int MOD::InfoPFC::vertex() const {
+    return _vertex;
+}
+
 
 const std::string MOD::InfoPFC::header() const {
 	stringstream ss;
